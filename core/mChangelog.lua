aaa3bc700725b76459de6100c12b5d41470ef58a
mBlinkii <mtvnow@gmx.de>
2022-12-18 15:26:13 +0100

    update version and changelog

--- core/mChangelog.lua
+++ core/mChangelog.lua
@@ -9,12 +9,12 @@ local format = format
 
 --Variables
 local ChangelogText =
-	"## [ver. 2.91] - 16.12.2022\n\n|CFFFF7F50### Update|r\n- |CFFDFFF00FIX|r NP-Healtmarker Auto range toggel not working\n- |CFFDFFF00FIX|r Custom Unitframe BG Textures\n- |CFFDFFF00FIX|r Profession Datatext Tooltip\n- |CFFDFFF00FIX|r Nil error in tooltip icons\n- |CFFDFFF00FIX|r Custom custom class color\n- |CFFDFFF00FIX|r Dock Icons Guild, Friends and Durability\n- |CFF40E0D0UPDATE|r Api change for intterupt on cd\n- |CFF40E0D0UPDATE|r Update Healthmarkers NPC IDs for DF S1\n- |CFF40E0D0UPDATE|r Add more functions for Dock Volume\n- |CFFDE3163REMOVE|r VolumeDisplay\n- |CFFDE3163REMOVE|r SL Currencys\n\n\n|CFFFF7F50### Added|r\n- |CFF6495EDNEW|r Statusbar Textures R - Series\n- |CFF6495EDNEW|r DF Currencys, Dragon Isles Supplies, Elemental Overflow\n- |CFF6495EDNEW|r Fonts Inter, Lemon, Oregano, Oswald, Ubuntu, Noto Sans\n- |CFF6495EDNEW|r Custom color for each Dock Icon\n- |CFF6495EDNEW|r Tooltip icon for mounts and toys in Retail\n- |CFF6495EDNEW|r Add more Tooltip infos for Dock Collection, Achievement\n- |CFF6495EDNEW|r Tag mLevelSmart:hideMax and mLevel:hideMax hides Player level on max level"
+	"## [ver. 2.91] - 16.12.2022\n\n|CFFFF7F50### Update|r\n- |CFFDFFF00FIX|r NP-Healtmarker Auto range toggel not working\n- |CFFDFFF00FIX|r Custom Unitframe BG Textures\n- |CFFDFFF00FIX|r Profession Datatext Tooltip\n- |CFFDFFF00FIX|r Nil error in tooltip icons\n- |CFFDFFF00FIX|r Custom custom class color\n- |CFFDFFF00FIX|r Dock Icons Guild, Friends and Durability\n- |CFF40E0D0UPDATE|r Api change for intterupt on cd\n- |CFF40E0D0UPDATE|r Update Healthmarkers NPC IDs for DF S1\n- |CFF40E0D0UPDATE|r Add more functions for Dock Volume\n- |CFFDE3163REMOVE|r VolumeDisplay\n- |CFFDE3163REMOVE|r SL Currencys\n\n\n|CFFFF7F50### Added|r\n- |CFF6495EDNEW|r Statusbar Textures R - Series\n- |CFF6495EDNEW|r DF Currencys, Dragon Isles Supplies, Elemental Overflow\n- |CFF6495EDNEW|r Fonts Inter, Lemon, Oregano, Oswald, Ubuntu, Noto Sans\n- |CFF6495EDNEW|r Custom color for each Dock Icon\n- |CFF6495EDNEW|r Tooltip icon for macros, mounts and toys in Retail\n- |CFF6495EDNEW|r Add more Tooltip infos for Dock Collection, Achievement\n- |CFF6495EDNEW|r Tag mLevelSmart:hideMax and mLevel:hideMax hides Player level on max level\n\n\n|CFFFF7F50### BUGFIX|r\n- |CFFDFFF00FIX|r Tooltip Spell Icons wrong Title"
 
 function mMT:Changelog(opt)
 	local Frame = CreateFrame("Frame", "mMediaTagChangelog", E.UIParent, "BackdropTemplate")
 	Frame:Point("CENTER")
-	Frame:Size(400, 500)
+	Frame:Size(400, 600)
 	Frame:CreateBackdrop("Transparent")
 	Frame:SetMovable(true)
 	Frame:EnableMouse(true)
@@ -24,12 +24,8 @@ function mMT:Changelog(opt)
 	Frame:SetClampedToScreen(true)
 	Frame.mLogo = Frame:CreateTexture(nil, "ARTWORK")
 	Frame.mLogo:Point("TOPLEFT", 72, -2)
-	Frame.mLogo:Point("BOTTOMRIGHT", -72, 434)
+	Frame.mLogo:Point("BOTTOMRIGHT", -72, 534)
 	Frame.mLogo:SetTexture("Interface\\AddOns\\ElvUI_mMediaTag\\media\\misc\\logo.tga")
-	Frame.UpperBar = Frame:CreateTexture(nil, "ARTWORK")
-	Frame.UpperBar:Point("TOPLEFT", 72, -2)
-	Frame.UpperBar:Point("BOTTOMRIGHT", -72, 434)
-	Frame.UpperBar:SetTexture("Interface\\AddOns\\ElvUI_mMediaTag\\media\\misc\\logo.tga")
 
 	local Font = GameFontHighlightSmall:GetFont()
 
@@ -40,8 +36,9 @@ function mMT:Changelog(opt)
 
 	local Label2 = Frame:CreateFontString("ChangelogText", "OVERLAY", "GameTooltipText")
 	Label2:SetFont(Font, 14)
-	Label2:SetPoint("TOPLEFT", 20, -120)
+	Label2:SetPoint("TOPLEFT", 20, -90)
 	Label2:SetWidth(360)
+	Label2:SetHeight(500)
 	Label2:SetText(ChangelogText)
 
 	local Close = CreateFrame("Button", "CloseButton", Frame, BackdropTemplateMixin and "BackdropTemplate")
