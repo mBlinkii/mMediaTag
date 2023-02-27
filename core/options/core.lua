0cba6a16cc6dc02caf3429b48459c874717eb587
mBlinkii <mtvnow@gmx.de>
2023-02-27 18:53:41 +0100

    test menu, logo and icons

--- core/options/core.lua
+++ core/options/core.lua
@@ -4,12 +4,34 @@ local  tinsert =  tinsert
 
 local function configTable()
 	E.Options.args.mMT = {
-		type = 'group',
+		type = "group",
 		name = mMT.Icon .. " " .. mMT.Name,
-		childGroups = "tab",
-		order = 10,
+		--childGroups = "tab",
+		order = 6,
+		--width = "full",
 		args = {
-
+			logo = {
+				type = "description",
+				name = "",
+				order = 1,
+				image = function() return "Interface\\Addons\\ElvUI_mMediaTag\\media\\logo\\mmt_logo.tga", 384, 96 end,
+			},
+			general = {
+				order = 2,
+				type = "group",
+				name = L["General"],
+				icon = "Interface\\Addons\\ElvUI_mMediaTag\\media\\system\\general.tga",
+				args = {
+				},
+			},
+			general2 = {
+				order = 22,
+				type = "group",
+				name = L["General"],
+				icon = "Interface\\Addons\\ElvUI_mMediaTag\\media\\system\\general.tga",
+				args = {
+				},
+			},
 		},
 	}
 end
