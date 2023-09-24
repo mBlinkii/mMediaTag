9bff9bf495d27bd6176dcfe3c435d057c88d797e
mBlinkii <mtvnow@gmx.de>
2023-09-24 15:56:47 +0200

    shorten code for bg texture

--- Addon/ElvUI_mMediaTag/modules/unitframes/background.lua
+++ Addon/ElvUI_mMediaTag/modules/unitframes/background.lua
@@ -2,35 +2,31 @@ local E = unpack(ElvUI)
 
 local UF = E:GetModule("UnitFrames")
 local LSM = LibStub("LibSharedMedia-3.0")
+
 local _G = _G
 local hooksecurefunc = _G.hooksecurefunc
 
-function CustomHealthBackdrop(frame)
+function CustomBackdrop(frame, texture)
 	if frame.backdropTex then
-		frame.backdropTex:SetTexture(LSM:Fetch("statusbar", E.db.mMT.custombackgrounds.health.texture))
+		frame.backdropTex:SetTexture(LSM:Fetch("statusbar", texture))
 		frame.backdropTex:ClearAllPoints()
 		frame.backdropTex:SetAllPoints()
 	end
+end
+
+function CustomHealthBackdrop(frame)
+	CustomBackdrop(frame, E.db.mMT.custombackgrounds.health.texture)
 end
 
 function CustomPowerBackdrop(frame)
-	if frame.backdropTex then
-		frame.backdropTex:SetTexture(LSM:Fetch("statusbar", E.db.mMT.custombackgrounds.power.texture))
-		frame.backdropTex:ClearAllPoints()
-		frame.backdropTex:SetAllPoints()
-	end
+	CustomBackdrop(frame, E.db.mMT.custombackgrounds.power.texture)
 end
 
 function CustomCastbarBackdrop(frame)
-	if frame.backdropTex then
-		frame.backdropTex:SetTexture(LSM:Fetch("statusbar", E.db.mMT.custombackgrounds.castbar.texture))
-		frame.backdropTex:ClearAllPoints()
-		frame.backdropTex:SetAllPoints()
-	end
+	CustomBackdrop(frame, E.db.mMT.custombackgrounds.castbar.texture)
 end
 
 function mMT:CustomBackdrop()
-	mMT:Print(E.db.mMT.custombackgrounds.health.enable, E.db.mMT.custombackgrounds.power.enable, E.db.mMT.custombackgrounds.castbar.enable)
 	if E.db.mMT.custombackgrounds.health.enable then
 		hooksecurefunc(UF, "PostUpdateHealthColor", CustomHealthBackdrop)
 	end
