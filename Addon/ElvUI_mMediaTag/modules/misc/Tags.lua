fc666695a4ec981f78d7df8bf5e27e644f1b5131
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2023-11-08 18:01:53 +0100

    Update Tags.lua

--- Addon/ElvUI_mMediaTag/modules/misc/Tags.lua
+++ Addon/ElvUI_mMediaTag/modules/misc/Tags.lua
@@ -1336,7 +1336,7 @@ E:AddTag("mQuestIcon", "QUEST_LOG_UPDATE", function(unit)
 	if UnitIsPlayer(unit) then
 		return
 	end
-	local isQuest = false --E.TagFunctions.GetQuestData(unit, "title")
+	local isQuest = E.TagFunctions.GetQuestData(unit, "title", Hex)
 	if isQuest then
 		return icons.quest
 	end
