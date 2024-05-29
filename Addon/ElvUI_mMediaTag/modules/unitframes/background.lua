0c78505af838b24a5b94e75c9d2f1c3b5929de31
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-05-29 19:56:52 +0200

    update custome bg texture functions to take care of custom textures

--- Addon/ElvUI_mMediaTag/modules/unitframes/background.lua
+++ Addon/ElvUI_mMediaTag/modules/unitframes/background.lua
@@ -10,40 +10,49 @@ end
 local _G = _G
 local hooksecurefunc = _G.hooksecurefunc
 
-local function CustomBackdrop(frame, texture)
-	if frame.backdropTex then
-		frame.backdropTex:SetTexture(LSM:Fetch("statusbar", texture))
-		frame.backdropTex:ClearAllPoints()
-		frame.backdropTex:SetAllPoints()
+local function CustomBackdrop(bar, texture)
+	if bar and bar.backdropTex then
+		bar.backdropTex:SetTexture(LSM:Fetch("statusbar", texture))
+		bar.backdropTex:ClearAllPoints()
+		bar.backdropTex:SetAllPoints()
 	end
 end
 
-local function CustomHealthBackdrop(frame)
-	CustomBackdrop(frame, E.db.mMT.custombackgrounds.health.texture)
+local function CustomHealthBackdrop(_, frame)
+	CustomBackdrop(frame and frame.Health, E.db.mMT.custombackgrounds.health.texture)
 end
 
-local function CustomPowerBackdrop(frame)
-	CustomBackdrop(frame, E.db.mMT.custombackgrounds.power.texture)
+local function CustomPowerBackdrop(_, frame)
+	CustomBackdrop(frame and frame.Power, E.db.mMT.custombackgrounds.power.texture)
 end
 
-local function CustomCastbarBackdrop(frame)
-	CustomBackdrop(frame, E.db.mMT.custombackgrounds.castbar.texture)
+local function CustomCastbarBackdrop(_, frame)
+	CustomBackdrop(frame and frame.Castbar, E.db.mMT.custombackgrounds.castbar.texture)
+end
+
+local function CustomAltPowerBackdrop(_, frame)
+	CustomBackdrop(frame and frame.AlternativePower, E.db.mMT.custombackgrounds.altpower.texture)
 end
 
 function module:Initialize()
 	if E.db.mMT.custombackgrounds.health.enable and not module.health then
-		hooksecurefunc(UF, "PostUpdateHealthColor", CustomHealthBackdrop)
+		hooksecurefunc(UF, "Configure_HealthBar", CustomHealthBackdrop)
 		module.health = true
 	end
 
 	if E.db.mMT.custombackgrounds.power.enable and not module.power then
-		hooksecurefunc(UF, "PostUpdatePowerColor", CustomPowerBackdrop)
+		hooksecurefunc(UF, "Configure_Power", CustomPowerBackdrop)
 		module.power = true
 	end
 
 	if E.db.mMT.custombackgrounds.castbar.enable and not module.castbar then
-		hooksecurefunc(UF, "PostCastStart", CustomCastbarBackdrop)
+		hooksecurefunc(UF, "Configure_Castbar", CustomCastbarBackdrop)
 		module.castbar = true
+	end
+
+	if E.db.mMT.custombackgrounds.altpower.enable and not module.altpower then
+		hooksecurefunc(UF, "Configure_AltPowerBar", CustomAltPowerBackdrop)
+		module.altpower = true
 	end
 
 	module.needReloadUI = true
