f5829eca431e535e56efd4344c5830620cd3feee
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-05-29 20:05:17 +0200

    update coede for uf textures

--- Addon/ElvUI_mMediaTag/modules/unitframes/uftextures.lua
+++ Addon/ElvUI_mMediaTag/modules/unitframes/uftextures.lua
@@ -12,52 +12,56 @@ local _G = _G
 local hooksecurefunc = _G.hooksecurefunc
 local db = nil
 
-local function SetCustomTexture(bar, texture)
-	bar:SetStatusBarTexture(texture)
-end
-
 local function SetBGTexture(bg, texture)
 	bg:SetTexture(texture)
 	bg:ClearAllPoints()
 	bg:SetAllPoints()
 end
 
-local function UpdateTextures(typ, frame)
-	--mMT:Print(frame.Power.backdropTex, frame.Power.bg)
+local function CustomHealthTexture(_, frame)
 	db = E.db.mMT.customtextures
-	local dbBG = E.db.mMT.custombackgrounds
-	local texture = nil
-	if frame then
-		if db.health.enable and frame.Health then
-			texture = LSM:Fetch("statusbar", db.health.texture)
-			SetCustomTexture(frame.Health, texture)
-			if not dbBG.health.enable and frame.Health.backdropTex then
-				SetBGTexture(frame.Health.backdropTex, texture)
-			end
-		end
+	local texture = LSM:Fetch("statusbar", db.health.texture)
 
-		if db.power.enable and frame.Power then
-			texture = LSM:Fetch("statusbar", db.power.texture)
-			SetCustomTexture(frame.Power, texture)
-			if not dbBG.power.enable and frame.Power.backdropTex then
-				SetBGTexture(frame.Power.backdropTex, texture)
-			end
+	if frame and frame.Health then
+		frame.Health:SetStatusBarTexture(texture)
+		if not E.db.mMT.custombackgrounds.health.enable and frame.Health.backdropTex then
+			SetBGTexture(frame.Health.backdropTex, texture)
 		end
+	end
+end
 
-		if db.castbar.enable and frame.Castbar then
-			texture = LSM:Fetch("statusbar", db.castbar.texture)
-			SetCustomTexture(frame.Castbar, texture)
-			if not dbBG.castbar.enable and frame.Castbar.backdropTex then
-				SetBGTexture(frame.Castbar.backdropTex, texture)
-			end
+local function CustomPowerTexture(_, frame)
+	db = E.db.mMT.customtextures
+	local texture = LSM:Fetch("statusbar", db.power.texture)
+
+	if frame and frame.Power then
+		frame.Power:SetStatusBarTexture(texture)
+		if not E.db.mMT.custombackgrounds.power.enable and frame.Power.backdropTex then
+			SetBGTexture(frame.Power.backdropTex, texture)
 		end
+	end
+end
 
-		if db.altpower.enable and frame.AlternativePower then
-			texture = LSM:Fetch("statusbar", db.altpower.texture)
-			SetCustomTexture(frame.AlternativePower, texture)
-			if not dbBG.altpower.enable and frame.AlternativePower.backdropTex then
-				SetBGTexture(frame.AlternativePower.backdropTex, texture)
-			end
+local function CustomCastbarTexture(_, frame)
+	db = E.db.mMT.customtextures
+	local texture = LSM:Fetch("statusbar", db.castbar.texture)
+
+	if frame and frame.Castbar then
+		frame.Castbar:SetStatusBarTexture(texture)
+		if not E.db.mMT.custombackgrounds.castbar.enable and frame.Castbar.backdropTex then
+			SetBGTexture(frame.Castbar.backdropTex, texture)
+		end
+	end
+end
+
+local function CustomAltPowerTexture(_, frame)
+	db = E.db.mMT.customtextures
+	local texture = LSM:Fetch("statusbar", db.altpower.texture)
+
+	if frame and frame.AlternativePower then
+		frame.AlternativePower:SetStatusBarTexture(texture)
+		if not E.db.mMT.custombackgrounds.altpower.enable and frame.AlternativePower.backdropTex then
+			SetBGTexture(frame.AlternativePower.backdropTex, texture)
 		end
 	end
 end
@@ -65,22 +69,22 @@ end
 function module:Initialize()
 	db = E.db.mMT.customtextures
 	if db.health.enable and not module.health then
-		hooksecurefunc(UF, "Configure_HealthBar", UpdateTextures)
+		hooksecurefunc(UF, "Configure_HealthBar", CustomHealthTexture)
 		module.health = true
 	end
 
 	if db.power.enable and not module.power then
-		hooksecurefunc(UF, "Configure_Power", UpdateTextures)
+		hooksecurefunc(UF, "Configure_Power", CustomPowerTexture)
 		module.power = true
 	end
 
 	if db.castbar.enable and not module.castbar then
-		hooksecurefunc(UF, "Configure_Castbar", UpdateTextures)
+		hooksecurefunc(UF, "Configure_Castbar", CustomCastbarTexture)
 		module.castbar = true
 	end
 
 	if db.altpower.enable and not module.altpower then
-		hooksecurefunc(UF, "Configure_AltPowerBar", UpdateTextures)
+		hooksecurefunc(UF, "Configure_AltPowerBar", CustomAltPowerTexture)
 		module.altpower = true
 	end
 
