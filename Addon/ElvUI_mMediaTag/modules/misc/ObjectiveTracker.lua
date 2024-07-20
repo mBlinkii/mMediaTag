4fd49ab6ffbf3b38a4b27180570fabd07ff62ca7
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-07-20 20:00:35 +0200

    🧽 UPDATE: TEST - BUILD own Objectivetracker

--- Addon/ElvUI_mMediaTag/modules/misc/ObjectiveTracker.lua
+++ Addon/ElvUI_mMediaTag/modules/misc/ObjectiveTracker.lua
@@ -8,8 +8,36 @@ if not module then
 	return
 end
 
+local questList = {
+	story = {},
+	quests = {},
+}
 
--- get quest infos
+local frame = CreateFrame("Frame", "mMT-QuestListFrame", UIParent)
+frame:SetSize(300, 500) -- Setzen Sie die Größe des Fensters
+frame:SetPoint("CENTER") -- Positionieren Sie das Fenster in der Mitte des Bildschirms
+
+-- title
+local title = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+title:SetPoint("TOP", 0, -10)
+title:SetText("Questliste")
+
+-- scroll frame
+local scrollFrame = CreateFrame("ScrollFrame", nil, frame, "UIPanelScrollFrameTemplate")
+scrollFrame:SetPoint("TOPLEFT", 10, -30)
+scrollFrame:SetPoint("BOTTOMRIGHT", -30, 10)
+
+-- text frame
+local textFrame = CreateFrame("Frame", nil, scrollFrame)
+scrollFrame:SetScrollChild(textFrame)
+textFrame:SetSize(scrollFrame:GetSize())
+
+-- text
+local text = textFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+text:SetPoint("TOPLEFT")
+text:SetJustifyH("LEFT")
+text:SetJustifyV("TOP")
+
 local function GetRequirements(text)
 	local current, required, questText = strmatch(text, "^(%d-)/(%d-) (.+)")
 	if not current or not required or not questText then
@@ -18,123 +46,159 @@ local function GetRequirements(text)
 	return current, required, questText
 end
 
-local function SetLineText(text, completed)
-	if not db then return end
-	local color = completed and db.font.color.complete or (db.font.color.text.class and mMT.ClassColor or db.font.color.text)
-	local fontsize = db.font.fontsize.text
+-- locallocal questOfferPinData =
+-- {
+-- 	[Enum.QuestClassification.Normal] = 	{ level = 1, atlas = "QuestNormal", },
+-- 	[Enum.QuestClassification.Questline] = 	{ level = 1, atlas = "QuestNormal", },
+-- 	[Enum.QuestClassification.Recurring] =	{ level = 2, atlas = "UI-QuestPoiRecurring-QuestBang", },
+-- 	[Enum.QuestClassification.Meta] = 		{ level = 3, atlas = "quest-wrapper-available", },
+-- 	[Enum.QuestClassification.Calling] = 	{ level = 4, atlas = "Quest-DailyCampaign-Available", },
+-- 	[Enum.QuestClassification.Campaign] = 	{ level = 5, atlas = "Quest-Campaign-Available", },
+-- 	[Enum.QuestClassification.Legendary] =	{ level = 6, atlas = "UI-QuestPoiLegendary-QuestBang", },
+-- 	[Enum.QuestClassification.Important] =	{ level = 7, atlas = "importantavailablequesticon", },
+-- };
 
-	text:SetFont(LSM:Fetch("font", db.font.font), fontsize, db.font.fontflag)
-	text:SetTextColor(color.r, color.g, color.b)
-	local lineText = text:GetText()
+local function SkinText(lineText)
+	local current, required, questText = GetRequirements(lineText)
 
-	if lineText then
-		-- Text Progress
-		if not completed then
-			local current, required, questText = GetRequirements(lineText)
-
-			if current and required and questText then
-				if current == required then
-					lineText = db.font.color.good.hex .. questText .. "|r"
-				else
-					local progressPercent = nil
-
-					local colorGood = db.font.color.good
-					local colorTransit = db.font.color.transit
-					local colorBad = db.font.color.bad
-
-					if required ~= "1" then
-						progressPercent = (tonumber(current) / tonumber(required)) * 100 or 0
-						local r, g, b = E:ColorGradient(progressPercent * 0.01, colorBad.r, colorBad.g, colorBad.b, colorTransit.r, colorTransit.g, colorTransit.b, colorGood.r, colorGood.g, colorGood.b)
-						local colorProgress = E:RGBToHex(r, g, b)
-						progressPercent = format("%.f%%", progressPercent)
-						lineText = colorProgress .. current .. "/" .. required .. " - " .. progressPercent .. "|r" .. "  " .. questText
-					else
-						lineText = db.font.color.bad.hex .. current .. "/" .. required .. "|r  " .. questText
-					end
-				end
-			end
+	if current and required and questText then
+		if current == required then
+			return "|cff40ff6e" .. questText .. "|r"
 		else
-			local _, _, questText = GetRequirements(lineText)
-			if questText then
-				lineText = db.font.color.good.hex .. questText .. "|r"
+			local progressPercent = nil
+
+			local colorGood = { r = 0.25, g = 1, b = 0.43, hex = "|cff40ff6e" }
+			local colorTransit = { r = 1, g = 0.63, b = 0.05, hex = "|cffffa10d" }
+			local colorBad = { r = 0.92, g = 0.46, b = 0.1, hex = "|cffeb751a" }
+
+			if required ~= "1" then
+				progressPercent = (tonumber(current) / tonumber(required)) * 100 or 0
+				local r, g, b = E:ColorGradient(progressPercent * 0.01, colorBad.r, colorBad.g, colorBad.b, colorTransit.r, colorTransit.g, colorTransit.b, colorGood.r, colorGood.g, colorGood.b)
+				local colorProgress = E:RGBToHex(r, g, b)
+				progressPercent = format("%.f%%", progressPercent)
+				return colorProgress .. current .. "/" .. required .. " - " .. progressPercent .. "|r" .. "  " .. questText
+			else
+				return "|cffeb751a" .. current .. "/" .. required .. "|r  " .. questText
 			end
 		end
-
-		text:SetText(lineText)
-		text:SetWordWrap(true)
-
-		return text:GetStringHeight()
+	else
+		return lineText
 	end
 end
 
-local function AddObjective(self, objectiveKey, text, template, useFullHeight, dashStyle, colorStyle, adjustForNoText, overrideHeight)
-	--mMT:Print(self, objectiveKey, text, template, useFullHeight, dashStyle, colorStyle, adjustForNoText, overrideHeight)
-	local line = self:GetLine(objectiveKey, template)
-
-	--mMT:DebugPrintTable(line,)
-    mMT:Print(line.state)
-	-- line.Text:SetText("UFFI")
-
-	if line and line.Text then
-		SetLineText(line.Text, (line.state == "Completed"))
+local function UpdateQuestList()
+	local quests = ""
+	local watchedQuests = {}
+	for i = 1, C_QuestLog.GetNumQuestWatches() do
+		local questID = C_QuestLog.GetQuestIDForQuestWatchIndex(i)
+		if questID then
+			watchedQuests[questID] = true
+		end
 	end
 
-	-- line.progressBar = nil;
+	local isTC = false
+	local isTQ = false
 
-	-- -- dash
-	-- if line.Dash then
-	-- 	if not dashStyle then
-	-- 		dashStyle = OBJECTIVE_DASH_STYLE_SHOW;
-	-- 	end
-	-- 	if line.dashStyle ~= dashStyle then
-	-- 		if dashStyle == OBJECTIVE_DASH_STYLE_SHOW then
-	-- 			line.Dash:Show();
-	-- 			line.Dash:SetText(QUEST_DASH);
-	-- 		elseif dashStyle == OBJECTIVE_DASH_STYLE_HIDE then
-	-- 			line.Dash:Hide();
-	-- 			line.Dash:SetText(QUEST_DASH);
-	-- 		elseif dashStyle == OBJECTIVE_DASH_STYLE_HIDE_AND_COLLAPSE then
-	-- 			line.Dash:Hide();
-	-- 			line.Dash:SetText(nil);
-	-- 		else
-	-- 			assertsafe(false, "Invalid dash style: " .. tostring(dashStyle));
-	-- 		end
-	-- 		line.dashStyle = dashStyle;
-	-- 	end
-	-- end
+	for i = 1, C_QuestLog.GetNumQuestLogEntries() do
+		local info = C_QuestLog.GetInfo(i)
+		mMT:DebugPrintTable(info)
 
-	--local lineSpacing = self.parentModule.lineSpacing;
-	-- local offsetY = -lineSpacing;
+		if info then
+			--	mMT:DebugPrintTable(info)
+			if not info.isHeader then
+				if info.questID ~= 0 and info.questClassification == Enum.QuestClassification.Campaign then
+					if not isTC then
+						quests = quests .. "|cff3fd4f2" .. TRACKER_HEADER_CAMPAIGN_QUESTS .. "|r\n" -- #ffc700
+						quests = quests .. "-------------------------\n"
+						isTC = true
+					end
 
-	-- -- anchor the line
-	-- local anchor = self.lastRegion or self.HeaderText;
-	-- if anchor then
-	-- 	line:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, offsetY);
-	-- else
-	-- 	line:SetPoint("TOPLEFT", 0, offsetY);
-	-- end
-	-- line:SetPoint("RIGHT", self.rightEdgeOffset, 0);
+					questList.story[info.questID] = {}
 
-	-- set the text
-	-- local textHeight = self:SetStringText(line.Text, text, useFullHeight, colorStyle, self.isHighlighted);
-	-- local height = overrideHeight or textHeight;
-	-- line:SetHeight(height);
+					local objectives = C_QuestLog.GetQuestObjectives(info.questID)
+					if objectives then
+						if watchedQuests[info.questID] then
+							quests = quests .. "|cffffc700" .. info.title .. "|r\n"
+						end
 
-	-- self.height = self.height + height + lineSpacing;
+						for line, objective in ipairs(objectives) do
+							questList.story[info.questID][line] = {
+								title = info.title,
+								finished = objective.finished,
+								progress = objective.percent,
+								text = objective.text,
+								watchedQuests = watchedQuests[info.questID],
+							}
+							if questList.story[info.questID][line].watchedQuests then
+								quests = quests .. SkinText(questList.story[info.questID][line].text) .. "\n"
+							end
+						end
 
-	-- self.lastRegion = line;
-	-- return line;
+						if watchedQuests[info.questID] then
+							quests = quests .. "\n"
+						end
+					end
+				else
+					if info.questID ~= 0 then
+						if not isTQ then
+							quests = quests .. "|cff7e3ff2" .. "QUEST" .. "|r\n"
+							quests = quests .. "-------------------------\n"
+							isTQ = true
+						end
 
-	-- line.Text
-end
+						questList.quests[info.questID] = {}
 
-local function AddBlock(self, block)
-	--mMT:DebugPrintTable(block)
+						local objectives = C_QuestLog.GetQuestObjectives(info.questID)
+						if objectives then
+							if watchedQuests[info.questID] then
+								quests = quests .. "|cffffc700" .. info.title .. "|r\n"
+							end
 
-	if block and block.AddObjective then
-		hooksecurefunc(block, "AddObjective", AddObjective)
+							for line, objective in ipairs(objectives) do
+								questList.quests[info.questID][line] = {
+									title = info.title,
+									finished = objective.finished,
+									progress = objective.percent,
+									text = objective.text,
+									watchedQuests = watchedQuests[info.questID],
+								}
+								if questList.quests[info.questID][line].watchedQuests then
+									quests = quests .. SkinText(questList.quests[info.questID][line].text) .. "\n"
+								end
+							end
+
+							if watchedQuests[info.questID] then
+								quests = quests .. "\n"
+							end
+						end
+					end
+				end
+
+				--mMT:DebugPrintTable(questList)
+			end
+		end
 	end
+
+	text:SetText(quests)
 end
+frame:RegisterEvent("QUEST_LOG_UPDATE")
+frame:SetScript("OnEvent", UpdateQuestList)
+
+
+UpdateQuestList()
+
+E:CreateMover(
+	frame,
+	"mMT-QuestListFrame-Mover",
+	"mMT-QuestList",
+	nil,
+	nil,
+	nil,
+	"ALL,MMEDIATAG",
+	nil,
+	"mMT,cosmetic,objectivetracker",
+	nil
+)
 
 function module:Initialize()
 	-- prevent bugs with wrong db entries
@@ -162,7 +226,7 @@ function module:Initialize()
 	end
 
 	db = E.db.mMT.objectivetracker
-
+	--SetTextColors()
 	if not module.hooked then
 		-- this hooked wont run?
 		-- QuestObjectiveTrackerMixin
@@ -171,11 +235,11 @@ function module:Initialize()
 		--mMT:DebugPrintTable(ObjectiveTrackerBlockMixin)
 		-- hooksecurefunc(_G.ObjectiveTrackerBlockMixin, "AddObjective", AddObjective)
 
-		if ObjectiveTrackerFrame and ObjectiveTrackerFrame.modules then
-			for _, m in pairs(ObjectiveTrackerFrame.modules) do
-				hooksecurefunc(m, "AddBlock", AddBlock)
-			end
-		end
+		-- if ObjectiveTrackerFrame and ObjectiveTrackerFrame.modules then
+		-- 	for _, m in pairs(ObjectiveTrackerFrame.modules) do
+		-- 		hooksecurefunc(m, "AddBlock", AddBlock)
+		-- 	end
+		-- end
 	end
 
 	module.needReloadUI = true
