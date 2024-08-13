c80c68b76c9cc87977939cff3de44782a504447b
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-08-13 19:28:21 +0200

    🧽 UPDATE: locales

--- Addon/ElvUI_mMediaTag/localization/deDE.lua
+++ Addon/ElvUI_mMediaTag/localization/deDE.lua
@@ -720,3 +720,11 @@ L["|cffFFFFFFControl + Left Click:|r Change Loadout"] = "|cffFFFFFFStrg + Linksk
 L["|cffFFFFFFLeft Click:|r Change Talent Specialization"] = "|cffFFFFFFLinks Klick:|r Talent Spezialisierung ändern"
 L["|cffFFFFFFRight Click:|r Change Loot Specialization"] = "|cffFFFFFFRechtsklick:|r Beutespezialisierung ändern"
 L["|cffFFFFFFShift + Left Click:|r Show Talent Specialization UI"] = "|cffFFFFFFShift + Links Klick:|r Zeige Talent Spezialisierungs Interface"
+
+-- new
+L["Released"] = true
+L["Version"] = true
+L["Important"] = true
+L["New"] = true
+L["Updates"] = true
+L["Fixes"] = true
