e3c95bf30467c918681af455c7e0d7643ac5c709
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-08-13 19:28:52 +0200

    🧽 UPDATE: CL

--- Addon/ElvUI_mMediaTag/core/changelog/3.51.lua
+++ Addon/ElvUI_mMediaTag/core/changelog/3.51.lua
@@ -11,30 +11,4 @@ mMT.Changelog[351] = {
         "[Portrait]: Boss Portraits cast icon toggle.",
         "[TAG]: Nil error with TAG targeting Icons."
 	},
-    NEW = {
-		"[Portrait]: some Bugs with code update.",
-        "[ObjectiveTracker]: Nil error in Dungeons.",
-        "[Portrait]: Boss Portraits cast icon toggle.",
-        "[TAG]: Nil error with TAG targeting Icons."
-	},
-    IMPORTANT = {
-		"[Portrait]: some Bugs with code update.",
-        "[ObjectiveTracker]: Nil error in Dungeons.",
-        "[Portrait]: Boss Portraits cast icon toggle.",
-        "[TAG]: Nil error with TAG targeting Icons."
-	},
 }
--- mMT.Changelog[351] = {
--- 	DATE = "xx.08.2024",
--- 	UPDATE = {
--- 		"[Portrait]: improved event Handler for Party.",
---         "[Portrait]: Party Portraits dont update correctly.",
---         "[System]: update License frame and Changelog menu.",
--- 	},
--- 	FIX = {
--- 		"[Portrait]: some Bugs with code update.",
---         "[ObjectiveTracker]: Nil error in Dungeons.",
---         "[Portrait]: Boss Portraits cast icon toggle.",
---         "[TAG]: Nil error with TAG targeting Icons."
--- 	},
--- }
