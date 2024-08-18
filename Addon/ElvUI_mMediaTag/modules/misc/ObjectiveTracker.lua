3aadda0761757be655afb2fc49d004f0dbb69150
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-08-18 16:20:57 +0200

    👌 IMPROVE: revert this

--- Addon/ElvUI_mMediaTag/modules/misc/ObjectiveTracker.lua
+++ Addon/ElvUI_mMediaTag/modules/misc/ObjectiveTracker.lua
@@ -168,8 +168,6 @@ local function SetTextProperties(text, fontSettings, color)
 	text:SetFont(LSM:Fetch("font", fontSettings.font), fontSettings.fontsize, fontSettings.fontflag)
 	if color then
 		text:SetTextColor(color.r, color.g, color.b)
-		text:SetShadowColor(0, 0, 0, 0)
-		text.SetShadowColor = E.noop
 	end
 end
 
