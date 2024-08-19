3803722e653e0d7289c1c6e956e6b9a8d17b2528
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-08-19 18:36:20 +0200

    🧽 UPDATE: CL & Version

---
+++ Addon/ElvUI_mMediaTag/core/changelog/3.53.lua
@@ -0,0 +1,26 @@
+mMT.Changelog[353] = {
+	DATE = "19.08.2024",
+	IMPORTANT = {
+		"[|CFFF64747REMOVED|r]: |CFFF88920Chatmenu Button|r no more used & you can use the right click menu of  the ElvUI Chat Copy button.", --#F88920
+		"[|CFFF64747REMOVED|r]: |CFFF88920QuestIcons|r no more needed & used.",
+	},
+	UPDATE = {
+		"[Datatext]: Update the code of some Datatext & Dock icons to use the improved method of Toxi (thank you Toxi).",
+		"[ObjectivTracker]: Header bar strata & level function.",
+		"[ObjectivTracker]: Will now a show check Icon if the line is complete.",
+		"[ObjectivTracker]: Will now not show requirements when the line is completed.",
+		"[Portrait]: Will now take care if the Player is in a Vehicle and shown as Pet frame.",
+		"[Roll Button]: You can now set a separat range for left & right click.",
+		"[System]: Notes to Settings to inform the users.",
+	},
+	FIX = {
+		"[DOCK]: Encounter Journal - Bug with the new API.",
+        "[DOCK]: Talents/Spec - Bug with the new API.",
+        "[ObjectivTracker]: Header bar Borders.",
+        "[ObjectivTracker]: Wrong complete trigger, which can show the lines as completed even if they are not yet completed.",
+        "[ObjectivTracker]: can sometimes show wrong requirements.",
+        "[Portrait]: Moon Shadow Texture.",
+        "[Portrait]: Nil error when ElvUI demo frames where toggled for Arena.",
+        "[System]: Fixed some more bugs with the new API.",
+	},
+}
