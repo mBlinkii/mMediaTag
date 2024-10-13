b5f2e69f070c240f2abc8d75018fc1d2938e314e
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-10-13 18:02:20 +0200

    🧽 UPDATE: locals

--- Addon/ElvUI_mMediaTag/localization/frFR.lua
+++ Addon/ElvUI_mMediaTag/localization/frFR.lua
@@ -804,3 +804,6 @@ L["|cffFFFFFFShift + Left Click:|r Show Talent Specialization UI"] = true
 
 -- new
 L["Aspect ratio"] = true
+L["Effect"] = true
+L["Cardinal Point"] = true
+L["Info! To achieve an optimal result with the minimap, a texture should be set for the texture and mask.\nThe mask is always required and no minimap will be visible without it.\n\n"] = true
