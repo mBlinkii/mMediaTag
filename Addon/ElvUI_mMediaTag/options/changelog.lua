333b7809f987c92b4cb2a0aa68ee291ae9c102dd
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-02-01 18:32:17 +0100

    🧷 UPDATE: changelog

--- Addon/ElvUI_mMediaTag/options/changelog.lua
+++ Addon/ElvUI_mMediaTag/options/changelog.lua
@@ -10,13 +10,15 @@ local type = type
 local mod = math.fmod
 
 local dash = "|TInterface\\Addons\\ElvUI_mMediaTag\\media\\options\\dash.tga:14:14|t  "
-local function Color(str)
-	if type(str) ~= "string" then str = tostring(str) end
-	return "[" .. "|CFF0393FF" .. str .. "|r" .. "]"
+local function Color(string)
+	if type(string) ~= "string" then string = tostring(string) end
+
+	return "[" .. "|CFF0393FF" .. string .. "|r" .. "]"
 end
 
 local function renderChangeLogLine(line)
-	return gsub(line, "%[([^%[]+)%]", Color)
+	line = gsub(line, "%[([^%[]+)%]", Color)
+	return line
 end
 
 mMT.options.args.changelog.args = {}
@@ -56,15 +58,104 @@ do
 			fontSize = "medium",
 		}
 
-		local order = 4
-		for _, line in ipairs(data.CHANGES) do
-			page["line" .. order] = {
-				order = order,
-				type = "description",
-				name = dash .. renderChangeLogLine(line),
-				fontSize = "medium",
+		local importantPart = data and data.IMPORTANT
+		if importantPart and #importantPart > 0 then
+			local iconImportant = "|TInterface\\Addons\\ElvUI_mMediaTag\\media\\options\\important.tga:14:14|t"
+			page.important = {
+				order = 4,
+				type = "group",
+				guiInline = true,
+				name = iconImportant .. "  |CFFFF006C" .. L["Important"] .. "|r",
+				args = {
+					text = {
+						order = 1,
+						type = "description",
+						fontSize = "medium",
+						name = function()
+							local text = ""
+							for _, line in ipairs(importantPart) do
+								text = text .. dash .. renderChangeLogLine(line) .. "\n"
+							end
+							return text .. "\n"
+						end,
+					},
+				},
 			}
-			order = order + 1
+		end
+
+		local newPart = data and data.NEW
+		if newPart and #newPart > 0 then
+			local iconNew = "|TInterface\\Addons\\ElvUI_mMediaTag\\media\\options\\new.tga:14:14|t"
+			page.new = {
+				order = 5,
+				type = "group",
+				guiInline = true,
+				name = iconNew .. "  |CFF6559F1" .. L["New"] .. "|r",
+				args = {
+					text = {
+						order = 1,
+						type = "description",
+						fontSize = "medium",
+						name = function()
+							local text = ""
+							for _, line in ipairs(newPart) do
+								text = text .. dash .. renderChangeLogLine(line) .. "\n"
+							end
+							return text .. "\n"
+						end,
+					},
+				},
+			}
+		end
+
+		local updatePart = data and data.UPDATE
+		if updatePart and #updatePart > 0 then
+			local iconUpdate = "|TInterface\\Addons\\ElvUI_mMediaTag\\media\\options\\update.tga:14:14|t"
+			page.update = {
+				order = 6,
+				type = "group",
+				guiInline = true,
+				name = iconUpdate .. "  |CFFBC26E5" .. L["Updates"] .. "|r",
+				args = {
+					text = {
+						order = 1,
+						type = "description",
+						fontSize = "medium",
+						name = function()
+							local text = ""
+							for _, line in ipairs(updatePart) do
+								text = text .. dash .. renderChangeLogLine(line) .. "\n"
+							end
+							return text .. "\n"
+						end,
+					},
+				},
+			}
+		end
+
+		local fixPart = data and data.FIX
+		if fixPart and #fixPart > 0 then
+			local iconFixe = "|TInterface\\Addons\\ElvUI_mMediaTag\\media\\options\\fix.tga:14:14|t"
+			page.fix = {
+				order = 7,
+				type = "group",
+				guiInline = true,
+				name = iconFixe .. "  |CFFFFA800" .. L["Fixes"] .. "|r",
+				args = {
+					text = {
+						order = 1,
+						type = "description",
+						fontSize = "medium",
+						name = function()
+							local text = ""
+							for _, line in ipairs(fixPart) do
+								text = text .. dash .. renderChangeLogLine(line) .. "\n"
+							end
+							return text .. "\n"
+						end,
+					},
+				},
+			}
 		end
 	end
 end
