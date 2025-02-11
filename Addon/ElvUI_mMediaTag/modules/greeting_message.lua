035a158ec5311e43b6794dc24c1c7d21a148bce9
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-02-11 17:54:59 +0100

    🧷 UPDATE: optm.

--- Addon/ElvUI_mMediaTag/modules/greeting_message.lua
+++ Addon/ElvUI_mMediaTag/modules/greeting_message.lua
@@ -3,18 +3,10 @@ local mMT, DB, M, E, P, L, MEDIA = unpack(ElvUI_mMediaTag)
 M.GreetingMessage = {}
 
 local function GreetingMessage()
-	print(
-		format(
-			L["Welcome to %s %s version |CFFF7DC6F%s|r, type |CFF58D68D/mmt|r to access the in-game configuration menu or type |CFF58D68D/mmt help|r for an overview of all chat commands."],
-			MEDIA.icon16,
-			mMT.Name,
-			mMT.Version
-		)
-	)
+	local text = L["Welcome to %s %s version |CFFF7DC6F%s|r, type |CFF58D68D/mmt|r to access the in-game configuration menu or type |CFF58D68D/mmt help|r for an overview of all chat commands."]
+	print(format(text, MEDIA.icon16, mMT.Name, mMT.Version))
 end
 
-M.GreetingMessage.Initialize = function()
-    if E.db.mMT.greeting_message then
-        GreetingMessage()
-    end
+function M.GreetingMessage:Initialize()
+	if E.db.mMT.greeting_message then GreetingMessage() end
 end
