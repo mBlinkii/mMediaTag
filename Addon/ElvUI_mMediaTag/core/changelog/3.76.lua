d38cb0745a97e9b731eeadf5e9616b7c55543c17
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-08-06 11:31:33 +0200

    🧷 UPDATE: cl

---
+++ Addon/ElvUI_mMediaTag/core/changelog/3.76.lua
@@ -0,0 +1,7 @@
+mMT.Changelog[376] = {
+	DATE = "06.07.2025",
+	IMPORTANT = { "Attention! With the next version 4.0 of mMT there will be some major changes that will reset some settings." },
+	FIX = {
+		"[TAG]: Prevent a nil Error with Class & Target icons on MOP",
+	},
+}
---
+++ Addon/ElvUI_mMediaTag/core/changelog/3.77.lua
@@ -0,0 +1,15 @@
+mMT.Changelog[377] = {
+	DATE = "06.08.2025",
+	IMPORTANT = { "Attention! With the next version 4.0 of mMT there will be some major changes that will reset some settings." },
+	FIX = {
+		"[TAGs]: Prevent a Nil error."
+	},
+    UPDATE = {
+        "[System]: TOC Game Version for Retail.",
+        "[Teleports-Datatext]: Add TWW S3 Teleports.",
+        "[Nameplate-Healthmarker]: Update for TWW S3.",
+    },
+    NEW = {
+        "[Class-Icons]: New Class Icons Set Simple.",
+    }
+}
