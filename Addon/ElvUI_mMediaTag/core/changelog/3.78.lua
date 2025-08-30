ae8617d2c7574419821254c00230290c859ef070
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-08-30 15:50:50 +0200

    🧷 UPDATE: cl

---
+++ Addon/ElvUI_mMediaTag/core/changelog/3.78.lua
@@ -0,0 +1,11 @@
+mMT.Changelog[378] = {
+	DATE = "30.08.2025",
+	IMPORTANT = { "Attention! With the next version 4.0 of mMT there will be some major changes that will reset some settings." },
+	FIX = {
+		"[Datatext-Crest]: Fixed Crest IDs."
+	},
+    UPDATE = {
+        "[System]: Update Boss IDs.",
+        "[System]: Update Instance IDs.",
+    },
+}
