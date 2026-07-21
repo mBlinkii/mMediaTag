ade47e002509f5d6b24133632bbf26338b75ff57
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2026-07-21 17:39:57 +0200

    uto friendly nameplates in dungeons/raids secret-safe unit classification improve: teleports datatext fix: auto quest turn-in

--- Addon/ElvUI_mMediaTag/options/changelog/new.lua
+++
@@ -1,9 +0,0 @@
-local mMT, DB, M, E, P, L, MEDIA = unpack(ElvUI_mMediaTag)
-
-mMT.Changelog[111]  = {
-	DATE = "Dev not",
-	IMPORTANT = { "there is nothing new here" },
-	NEW = { "there is nothing new here" },
-	UPDATE = { "there is nothing new here" },
-	FIX = { "there is nothing new here" },
-}
